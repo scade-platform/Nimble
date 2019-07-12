0726953acc4051268807c92ffa92088725bdc4a7
shem <vsv@scade.io>
2019-07-12 18:14:39 +0200

    add ScadeKitExtensions framework for hiding dependenices to ScadeSDK

---
+++ Plugins/InterfaceBuilder/InterfaceBuilder/ScadeKitExtensions/ScadeKitExtensions.h
@@ -0,0 +1,19 @@
+//
+//  ScadeKitExtensions.h
+//  ScadeKitExtensions
+//
+//  Created by shem on 12/07/2019.
+//  Copyright © 2019 SCADE. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ScadeKitExtensions.
+FOUNDATION_EXPORT double ScadeKitExtensionsVersionNumber;
+
+//! Project version string for ScadeKitExtensions.
+FOUNDATION_EXPORT const unsigned char ScadeKitExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ScadeKitExtensions/PublicHeader.h>
+
+
---
+++ Plugins/InterfaceBuilder/InterfaceBuilder/Sources/InterfaceBuilder.mm
@@ -0,0 +1,9 @@
+//
+//  Fake.m
+//  InterfaceBuilder
+//
+//  Created by shem on 12/07/2019.
+//  Copyright © 2019 SCADE. All rights reserved.
+//
+
+//#import <Foundation/Foundation.h>
