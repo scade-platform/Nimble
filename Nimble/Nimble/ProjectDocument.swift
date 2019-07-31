f3e024d191461c0ba0f9fccf260afea634abf2da
Danil Kristalev <kristalev.danil@gmail.com>
2019-07-31 14:36:12 +0700

    Configure document based application

---
+++ Nimble/Nimble/ProjectDocument.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectDocument.swift
+//  Nimble
+//
+//  Created by Danil Kristalev on 31/07/2019.
+//  Copyright © 2019 SCADE. All rights reserved.
+//
+
+import AppKit
+
+class ProjectDocument : NSDocument {
+  
+  override init() {
+    super.init()
+  }
+  
+}
