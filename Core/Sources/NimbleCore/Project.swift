d6e65922f84f78451b7ec5524a5485ce57cfa619
Danil Kristalev <kristalev.danil@gmail.com>
2019-08-02 10:23:05 +0700

    [NIM-8] Project file loader

--- Core/Sources/NimbleCore/Project.swift
+++ Core/Sources/NimbleCore/Project.swift
@@ -6,10 +6,12 @@
 //  Copyright © 2019 SCADE. All rights reserved.
 //
 
+import Cocoa
+import Yams
 
 public class Project {
   //TODO: remove default home path, it's only for demo
-  public var folders: [Folder] = [Folder(path: Path.home)]
+  public var folders: [Folder] = []
 }
 
 
@@ -17,4 +19,18 @@ public class ProjectManager {
   public var currentProject: Project = Project()
   
   public static let shared: ProjectManager = ProjectManager()
+}
+
+extension Project{
+  public func read(from data: Data){
+    let yamlContent = String(bytes: data, encoding: .utf8)!
+    let loadedDictionary = try? Yams.load(yaml: yamlContent) as! [String: [String]]
+    if let loadedDictionary = loadedDictionary, let foldersPaths = loadedDictionary["Folders"] {
+      folders = foldersPaths.map{Folder(path: $0)!}
+    }
+  }
+  
+  public func data() -> Data? {
+    return Data()
+  }
 }
