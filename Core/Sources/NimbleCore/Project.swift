8b94c5874a05e7fb4f12c9b2c63342b25bdc1e56
Danil Kristalev <kristalev.danil@gmail.com>
2019-08-05 20:02:25 +0700

    Add case insensitive subscript for dictionary

--- Core/Sources/NimbleCore/Project.swift
+++ Core/Sources/NimbleCore/Project.swift
@@ -35,7 +35,7 @@ extension Project{
       return nil
     }
     let loadedDictionary = try? Yams.load(yaml: yamlContent) as! [String: [String]]
-    if let loadedDictionary = loadedDictionary, let foldersPaths = loadedDictionary["Folders"] {
+    if let loadedDictionary = loadedDictionary, let foldersPaths = loadedDictionary[caseInsensitive: "FOLDERS"] {
       var incorrectPaths = [String]()
       for folderPath in foldersPaths {
         if let folder = Folder(path: folderPath) {
@@ -56,4 +56,24 @@ extension Project{
   public func data() -> Data {
     return Data()
   }
+}
+
+private extension Dictionary where Key == String {
+  
+  subscript(caseInsensitive key: Key) -> Value? {
+    get {
+      if let k = keys.first(where: { $0.caseInsensitiveCompare(key) == .orderedSame }) {
+        return self[k]
+      }
+      return nil
+    }
+    set {
+      if let k = keys.first(where: { $0.caseInsensitiveCompare(key) == .orderedSame }) {
+        self[k] = newValue
+      } else {
+        self[key] = newValue
+      }
+    }
+  }
+  
 }
