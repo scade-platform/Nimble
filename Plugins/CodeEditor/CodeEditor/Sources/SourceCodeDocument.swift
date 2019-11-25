d7472bf86c4bc09fa4ad7f18662f8ac40c1bc81c
Grigory Markin <gr.markin@gmail.com>
2019-11-26 00:42:41 +0100

    provide a default UTI for a new file; extend supported UTIs by the code editor to "public.data"

--- Plugins/CodeEditor/CodeEditor/Sources/SourceCodeDocument.swift
+++ Plugins/CodeEditor/CodeEditor/Sources/SourceCodeDocument.swift
@@ -74,7 +74,7 @@ public final class SourceCodeDocument: NimbleDocument {
 
 extension SourceCodeDocument: Document {
   public var contentViewController: NSViewController? { return editorController }
-  public static var typeIdentifiers: [String] { ["public.text", "public.svg-image"] }
+  public static var typeIdentifiers: [String] { ["public.text", "public.data", "public.svg-image"] }
 }
 
 extension SourceCodeDocument: TextDocument {
@@ -84,6 +84,6 @@ extension SourceCodeDocument: TextDocument {
 extension SourceCodeDocument: CreatableDocument {
   public static let newMenuTitle: String = "File"
   public static func createUntitledDocument() -> Document? {
-    return SourceCodeDocument()
+    return try? SourceCodeDocument(type: "public.text")
   }
 }
