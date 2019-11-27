b099b107a1dd708d6c1c79ceb8a70aa7b49a5b55
Danil Kristalev <kristalev.danil@gmail.com>
2019-11-27 18:31:23 +0700

    Add NSFilePresenter implementation to FileSystemElement, add FileSystemElementObserver protocol, add ObserverSetDelegate protocol implementation to FileSystemElement to start and stop FS observing

--- Nimble/NimbleCore/Sources/NimbleCore/File.swift
+++ Nimble/NimbleCore/Sources/NimbleCore/File.swift
@@ -18,6 +18,9 @@ public class File: FileSystemElement {
 
 
 public class FileSystemElement {
+  public var observers = ObserverSet<FileSystemElementObserver>()
+  private var innerFileObserver: InnerFileObserver?
+  
   public let path: Path
   
   public var url: URL {
@@ -38,6 +41,7 @@ public class FileSystemElement {
   
   public init?(path: Path) {
     self.path = path
+    self.observers.delegate = self
   }
       
   public convenience init?(path: String) {
@@ -49,6 +53,11 @@ public class FileSystemElement {
     guard let path = Path(url: url) else { return nil }
     self.init(path: path)
   }
+  
+  deinit {
+    guard let filePresenter = innerFileObserver else { return }
+    NSFileCoordinator.removeFilePresenter(filePresenter)
+  }
 }
 
 
@@ -59,6 +68,60 @@ extension FileSystemElement: Hashable {
 
   public func hash(into hasher: inout Hasher) {
     hasher.combine(path)
+  }
+}
+
+extension FileSystemElement : ObserverSetDelegate {
+  
+  public func observerWillAdd(_ observer: AnyObject) {
+    //begin FS observing only if there is at least one observer
+    guard innerFileObserver == nil else { return }
+    let filePresenter = InnerFileObserver(self, observers: observers)
+    self.innerFileObserver = filePresenter
+    NSFileCoordinator.addFilePresenter(filePresenter)
+  }
+  
+  public func observerDidRemove(_ observer: AnyObject) {
+    stopInnerObserver()
+  }
+  
+  public func observerDidRelease() {
+    stopInnerObserver()
+  }
+  
+  private func stopInnerObserver(){
+    //stop FS observing if there aren't observers
+    guard observers.isEmpty, let filePresenter = innerFileObserver else { return }
+    NSFileCoordinator.removeFilePresenter(filePresenter)
+    innerFileObserver = nil
+  }
+}
+
+fileprivate extension FileSystemElement {
+  class InnerFileObserver: NSObject, NSFilePresenter {
+    var presentedItemURL: URL?
+    var presentedItemOperationQueue: OperationQueue {
+      return OperationQueue.main
+    }
+    
+    let observers: ObserverSet<FileSystemElementObserver>
+    let fileSystemElement: FileSystemElement
+    
+    
+    init(_ fileSystemElement: FileSystemElement, observers: ObserverSet<FileSystemElementObserver> ) {
+      self.fileSystemElement = fileSystemElement
+      self.presentedItemURL = fileSystemElement.path.url
+      self.observers = observers
+      super.init()
+    }
+    
+    func presentedSubitemDidChange(at url: URL) {
+      observers.notify{$0.subitemDidChange(fileSystemElement, subitem: url)}
+    }
+    
+    func presentedItemDidChange() {
+      observers.notify{$0.fileSystemElementDidChange(fileSystemElement)}
+    }
   }
 }
 
@@ -85,4 +148,15 @@ public extension URL {
   func typeIdentifierConforms(to: String) -> Bool {
     return UTTypeConformsTo(uti as CFString , to as CFString)
   }    
+}
+
+public protocol FileSystemElementObserver {
+  func fileSystemElementDidChange(_ fileSystemElement: FileSystemElement)
+  func subitemDidChange(_ fileSystemElement: FileSystemElement, subitem: URL)
+}
+
+public extension FileSystemElementObserver {
+  //default implementation
+  func fileSystemElementDidChange(_ fileSystemElement: FileSystemElement) {}
+  func subitemDidChange(_ fileSystemElement: FileSystemElement, subitem: URL) {}
 }
