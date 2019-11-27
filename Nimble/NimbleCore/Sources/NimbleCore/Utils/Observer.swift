8da7070c59895c79349cf04a60e13827326636ff
Danil Kristalev <kristalev.danil@gmail.com>
2019-11-27 17:27:59 +0700

    Add observer set delegate

--- Nimble/NimbleCore/Sources/NimbleCore/Utils/Observer.swift
+++ Nimble/NimbleCore/Sources/NimbleCore/Utils/Observer.swift
@@ -14,29 +14,40 @@ fileprivate struct ObserverRef<T> {
 public class ObserverSet<T> {
   fileprivate var observers: [ObjectIdentifier: ObserverRef<T>] = [:]
   
+  var delegate: ObserverSetDelegate?
+  
   public init() { }
   
   public func add(observer: T) {
     let obj = observer as AnyObject
     let key = ObjectIdentifier(obj)
+    delegate?.observerWillAdd(obj)
     observers[key] = ObserverRef<T>(object: obj)
+    delegate?.observerDidAdd(obj)
   }
   
   public func remove(observer: T) {
     // Casting to AnyObject always succeeds, even for enum types
     let obj = observer as AnyObject
     let key = ObjectIdentifier(obj)
+    delegate?.observerWillRemove(obj)
     observers.removeValue(forKey: key)
+    delegate?.observerDidRemove(obj)
   }
   
   public func notify(with notifier: (T) -> Void) {
     for (id, ref) in observers {
       guard let observer = ref.value else {
         observers.removeValue(forKey: id)
+        delegate?.observerDidRelease()
         continue
       }
       notifier(observer)
     }
+  }
+  
+  public var isEmpty : Bool {
+    return observers.isEmpty
   }
 }
 
@@ -44,4 +55,21 @@ public class ObserverSet<T> {
 public protocol Observable {
   associatedtype ObserverType  
   var observers: ObserverSet<ObserverType> { get }
+}
+
+public protocol ObserverSetDelegate {
+  func observerWillAdd(_ observer: AnyObject)
+  func observerDidAdd(_ observer: AnyObject)
+  func observerWillRemove(_ observer: AnyObject)
+  func observerDidRemove(_ observer: AnyObject)
+  func observerDidRelease()
+}
+
+public extension ObserverSetDelegate {
+  //default implementation
+  func observerWillAdd(_ observer: AnyObject) {}
+  func observerDidAdd(_ observer: AnyObject) {}
+  func observerWillRemove(_ observer: AnyObject) {}
+  func observerDidRemove(_ observer: AnyObject) {}
+  func observerDidRelease(){}
 }
