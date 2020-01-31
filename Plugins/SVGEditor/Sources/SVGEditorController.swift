0df788c7e1265ee812b418132c569e83f76948c7
shem <vsv@scade.io>
2020-01-31 16:44:38 +0200

    add rendering of svg document at the center of editor view

--- Plugins/SVGEditor/Sources/SVGEditorController.swift
+++ Plugins/SVGEditor/Sources/SVGEditorController.swift
@@ -4,25 +4,29 @@ import NimbleCore
 
 class SVGEditorController: NSViewController {
 
-  @IBOutlet
-  weak var docView: NSView? = nil
-  
+  let maxSize: CGFloat = 400
+
   weak var doc: SVGDocument? = nil
 
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    //view.setBackgroundColor(NSColor.red)
-
     doc?.observers.add(observer: self)
 
-    guard let phoenixView = ScadeKitExtensions.createPhoenixView() else { return }
-    view.addSubview(phoenixView)
+    guard let svgView = ScadeKitExtensions.createPhoenixView() else { return }
+    
+    view.addSubview(svgView)
 
-    phoenixView.frame = NSMakeRect(0, 0, 100, 100)
+    svgView.translatesAutoresizingMaskIntoConstraints = false
+
+    svgView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    svgView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+
+    svgView.widthAnchor.constraint(equalToConstant: maxSize).isActive = true
+    svgView.heightAnchor.constraint(equalToConstant: maxSize).isActive = true
 
     if let root = doc?.svgRoot {
-      SCDRuntime.renderSvg(root, x: 0, y: 0, size: phoenixView.frame.size)
+      SCDRuntime.renderSvg(root, x: 0, y: 0, size: NSMakeSize(maxSize, maxSize))
     }
   }
 }
