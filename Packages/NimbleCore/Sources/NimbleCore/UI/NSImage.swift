d332f7b6eafd6e441e76ef8847d488af0648a7c1
shem <vsv@scade.io>
2020-03-01 17:33:22 +0200

    add svg redering extension to NSImage

--- Packages/NimbleCore/Sources/NimbleCore/UI/NSImage.swift
+++ Packages/NimbleCore/Sources/NimbleCore/UI/NSImage.swift
@@ -7,8 +7,10 @@
 //
 
 import Cocoa
+import SwiftSVG
 
 public extension NSImage {
+
   func imageWithTint(_ tint: NSColor) -> NSImage {
     var imageRect = NSZeroRect;
     imageRect.size = self.size;
@@ -25,5 +27,38 @@ public extension NSImage {
     highlightImage.unlockFocus()
     
     return highlightImage;
+  }
+
+  class func createSVG(from url: URL, bounds: NSRect,
+                       completion: @escaping (NSImage) -> ()) {
+
+    CALayer(svgURL: url) { layer in
+      guard let btmpImgRep =
+      NSBitmapImageRep(bitmapDataPlanes: nil,
+                       pixelsWide: Int(bounds.width),
+                       pixelsHigh: Int(bounds.height),
+                       bitsPerSample: 8,
+                       samplesPerPixel: 4,
+                       hasAlpha: true,
+                       isPlanar: false,
+                       colorSpaceName: .deviceRGB,
+                       bytesPerRow: 0,
+                       bitsPerPixel: 0) else { return }
+
+      guard let context = NSGraphicsContext(bitmapImageRep: btmpImgRep) else { return }
+
+      let xScale = bounds.width / layer.boundingBox.width
+      let yScale = bounds.height / layer.boundingBox.height
+
+      context.cgContext.translateBy(x: 0, y: bounds.height)
+      context.cgContext.scaleBy(x: xScale, y: -1 * yScale)
+
+      layer.render(in: context.cgContext)
+
+      let image = NSImage(size: bounds.size)
+      image.addRepresentation(btmpImgRep)
+
+      completion(image)
+    }
   }
 }
