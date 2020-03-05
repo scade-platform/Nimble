41aa523ed18685ce25f93a08dd9a235d76ff66c6
shem <vsv@scade.io>
2020-03-05 11:33:44 +0200

    add resource rendering from the page file

--- Plugins/InterfaceBuilder/InterfaceBuilder/Sources/InterfaceBuilder.swift
+++ Plugins/InterfaceBuilder/InterfaceBuilder/Sources/InterfaceBuilder.swift
@@ -14,19 +14,8 @@ public final class InterfaceBuilder: Module {
 }
 
 final class InterfaceBuilderPlugin: Plugin {
+
   func load() {
     DocumentManager.shared.registerDocumentClass(PageDocument.self)
   }
-  
-  public func activate(in workbench: Workbench) {
-    ///TODO: do this every time a project from the workbench has been changed
-    
-    workbench.project?.folders.forEach {
-      UserDefaults.standard.set($0.path.string, forKey: "Resource Folder")
-    }
-  }
-  
-  // public func deactivate(in workbench: Workbench) {
-  //   ///TODO: remove defaults
-  // }
 }
