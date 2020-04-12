743a6dc5da8ac0d569a3ad39d0fc4638331fa3d6
Grigory Markin <gr.markin@gmail.com>
2020-04-12 17:59:24 +0200

    replace run and stop button icons by SVGs; comment out custom drawing of tool bar images (as it reduces rendering quality)

--- Plugins/BuildSystem/Sources/BuildSystemModule.swift
+++ Plugins/BuildSystem/Sources/BuildSystemModule.swift
@@ -51,9 +51,13 @@ final class BuildSystemPlugin: Plugin {
   }
   
   private func setupCommands() {
-    let buttonIconColor = getColorFromAsset("ButtonIconColor", defualt: .darkGray)
-    let runImage = Bundle(for: BuildSystemPlugin.self).image(forResource: "run")?.imageWithTint(buttonIconColor)
-    let stopImage = Bundle(for: BuildSystemPlugin.self).image(forResource: "stop")?.imageWithTint(buttonIconColor)
+    //let buttonIconColor = getColorFromAsset("ButtonIconColor", defualt: .darkGray)
+    
+    let runImage = IconsManager.Icons.run.image()
+    let stopImage = IconsManager.Icons.stop.image()
+    
+    //let runImage = Bundle(for: BuildSystemPlugin.self).image(forResource: "run")?.imageWithTint(buttonIconColor)
+    //let stopImage = Bundle(for: BuildSystemPlugin.self).image(forResource: "stop")?.imageWithTint(buttonIconColor)
 
     runCommand = Command(name: "Run", menuPath: "Tools", keyEquivalent: "cmd+r", toolbarIcon: runImage) { _ in self.run() }
     CommandManager.shared.registerCommand(command: runCommand!)
