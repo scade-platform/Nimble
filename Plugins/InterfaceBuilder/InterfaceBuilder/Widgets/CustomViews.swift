8b237d0f9c72e3e7d665d471687b748844b715f6
Danil Kristalev <kristalev.danil@gmail.com>
2020-04-16 16:16:20 +0700

    Move custom views to CustomView file, add PaddedTextFieldCell to set the padding for a rounded text field

---
+++ Plugins/InterfaceBuilder/InterfaceBuilder/Widgets/CustomViews.swift
@@ -0,0 +1,77 @@
+//
+//  CustomViews.swift
+//  InterfaceBuilder
+//
+//  Created by Danil Kristalev on 16.04.2020.
+//  Copyright © 2020 SCADE. All rights reserved.
+//
+
+import Cocoa
+import NimbleCore
+
+class ColorView: NSView {
+  required init?(coder: NSCoder) {
+    super.init(coder: coder)
+    self.wantsLayer = true
+    let layer = CALayer()
+    layer.masksToBounds = true
+    layer.cornerRadius = 5.5
+    layer.borderWidth = 0.5
+    layer.borderColor = ColorView.sharedBorderColor.cgColor
+    self.layer = layer
+  }
+  
+  override func layout() {
+    if let layer = self.layer {
+      //change border color after system theme changed
+      layer.borderColor = ColorView.sharedBorderColor.cgColor
+    }
+  }
+  
+  private static var sharedBorderColor: NSColor {
+    switch Theme.Style.system {
+    case .dark:
+      return NSColor(colorCode: "#303030")!
+    case .light:
+      return NSColor(colorCode: "#B1B1B1")!
+    }
+  }
+}
+
+
+class HeaderView: NSView {
+  var trackingArea: NSTrackingArea? = nil
+  weak var button : NSButton?
+
+  override func updateTrackingAreas() {
+    if let ta = trackingArea {
+      self.removeTrackingArea(ta)
+    }
+
+    let opts: NSTrackingArea.Options = [.mouseEnteredAndExited, .activeAlways]
+    trackingArea = NSTrackingArea(rect: self.bounds, options: opts, owner: self)
+
+    self.addTrackingArea(trackingArea!)
+  }
+
+  open override func mouseEntered(with theEvent: NSEvent) {
+    if button?.title == "Hide" {
+      button?.isHidden = false
+    }
+  }
+
+  open override func mouseExited(with event: NSEvent) {
+    if button?.title == "Hide" {
+      button?.isHidden = true
+    }
+  }
+}
+
+class PaddedTextFieldCell: NSTextFieldCell {
+    @IBInspectable var leftPadding: CGFloat = 10.0
+  
+    override func drawingRect(forBounds rect: NSRect) -> NSRect {
+      let rectInset = NSMakeRect(rect.origin.x + leftPadding, rect.origin.y, rect.size.width - leftPadding, rect.size.height)
+      return super.drawingRect(forBounds: rectInset)
+    }
+}
--- Plugins/InterfaceBuilder/InterfaceBuilder/Widgets/TextPane.swift
+++ Plugins/InterfaceBuilder/InterfaceBuilder/Widgets/TextPane.swift
@@ -135,62 +135,7 @@ class TextPane: NSViewController {
   }
 }
 
-class ColorView: NSView {
-  required init?(coder: NSCoder) {
-    super.init(coder: coder)
-    self.wantsLayer = true
-    let layer = CALayer()
-    layer.masksToBounds = true
-    layer.cornerRadius = 5.5
-    layer.borderWidth = 0.5
-    layer.borderColor = ColorView.sharedBorderColor.cgColor
-    self.layer = layer
-  }
-  
-  override func layout() {
-    if let layer = self.layer {
-      //change border color after system theme changed
-      layer.borderColor = ColorView.sharedBorderColor.cgColor
-    }
-  }
-  
-  private static var sharedBorderColor: NSColor {
-    switch Theme.Style.system {
-    case .dark:
-      return NSColor(colorCode: "#303030")!
-    case .light:
-      return NSColor(colorCode: "#B1B1B1")!
-    }
-  }
-}
 
-class HeaderView: NSView {
-  var trackingArea: NSTrackingArea? = nil
-  weak var button : NSButton?
-
-  override func updateTrackingAreas() {
-    if let ta = trackingArea {
-      self.removeTrackingArea(ta)
-    }
-
-    let opts: NSTrackingArea.Options = [.mouseEnteredAndExited, .activeAlways]
-    trackingArea = NSTrackingArea(rect: self.bounds, options: opts, owner: self)
-
-    self.addTrackingArea(trackingArea!)
-  }
-
-  open override func mouseEntered(with theEvent: NSEvent) {
-    if button?.title == "Hide" {
-      button?.isHidden = false
-    }
-  }
-
-  open override func mouseExited(with event: NSEvent) {
-    if button?.title == "Hide" {
-      button?.isHidden = true
-    }
-  }
-}
 
 extension TextPane : EditorViewObserver {
   func editorDidChangeSelection(editor: EditorView, widget: SCDWidgetsWidget) {
