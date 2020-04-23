b908013f35cbb60f7d44578230176057e28d0cd5
shem <vsv@scade.io>
2020-04-23 16:20:44 +0200

    update ScadeKit framework

--- Plugins/SVGEditor/Libraries/ScadeKit.framework/Versions/A/Headers/SCDLatticeEditorPageAdapter.h
+++ Plugins/SVGEditor/Libraries/ScadeKit.framework/Versions/A/Headers/SCDLatticeEditorPageAdapter.h
@@ -16,6 +16,11 @@ SCADE_API
 @interface SCDLatticeEditorPageAdapter : SCDLatticePageAdapter
 
 
+- (void)save:(NSString* _Nonnull)path;
+
+- (void)loadTemplatePage;
+
+
 /*PROTECTED REGION ID(db41d41a54cbc8e4587162ffa745d0a3) START*/
 // Please, enable the protected region if you add manually written code.
 // To do this, add the keyword ENABLED before START.
