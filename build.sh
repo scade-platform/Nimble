31c5181a904c75e4d3c3ec880c7c18a36e291fa9
Grigory Markin <gr.markin@gmail.com>
2020-04-26 20:23:27 +0200

    move the build logic to the shell script; reorganise workflows

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#######################################################################################
+# Exit when any command fails
+set -e
+# Keep track of the last executed command
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+# Echo an error message before exiting
+trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+#######################################################################################
+
+# Clean build directories
+find ~/Library/Developer/Xcode/DerivedData -type d -name "Nimble-*" -exec rm -rf {}/Build \;
+
+# Build project
+xcodebuild -workspace Nimble.xcworkspace -scheme Nimble -configuration=Release -xcconfig build.xcconfig install
+
+# Compress
+hdiutil create -volname Nimble -srcfolder ./Applications -ov -format UDZO Nimble.dmg
