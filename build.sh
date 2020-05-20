6b86bd7ae3c28b248abb2f3a2d87167b4b6e62c2
Grigory Markin <gr.markin@gmail.com>
2020-05-20 19:03:23 +0200

    add setting the version number by building a release

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 find ~/Library/Developer/Xcode/DerivedData -type d -name "Nimble-*" -exec rm -rf {}/Build \;
 
 # Build project
-xcodebuild -workspace Nimble.xcworkspace -scheme Nimble -configuration=Release -xcconfig build.xcconfig install
+xcodebuild -workspace Nimble.xcworkspace -scheme Nimble -configuration=Release -xcconfig build.xcconfig CURRENT_PROJECT_VERSION=$TAG_VERSION install
 
 # Compress
 hdiutil create -volname Nimble -srcfolder ./Applications -ov -format UDZO Nimble.dmg
