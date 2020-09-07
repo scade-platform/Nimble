17bebf9f731c31809dea70dc6918318303654da3
Kristalev Danil <kristalev.danil@gmail.com>
2020-09-07 19:26:28 +0700

    [NM-470] Add a "wizard" API to Nimble for creating new resources (#152)
    * [NM-470] Add a "wizard" API to Nimble for creating new resources/projects
    
    * Rename Generator to CreationWizard, refactoring

---
+++ Nimble/Sources/Controls/Wizard/WizardView.swift
@@ -0,0 +1,233 @@
+//
+//  WizardViewController.swift
+//  Nimble
+//
+//  Created by Danil Kristalev on 26.08.2020.
+//  Copyright © 2020 SCADE. All rights reserved.
+//
+
+import Cocoa
+import NimbleCore
+
+class WizardView: NSViewController {
+  
+  @IBOutlet weak var outline: NSOutlineView?
+  
+  @IBOutlet weak var label: NSTextField?
+  
+  @IBOutlet weak var previousButton: NSButton?
+  @IBOutlet weak var nextButton: NSButton?
+  
+  @IBOutlet weak var mainTabViewItem: NSTabViewItem!
+  @IBOutlet weak var tabView: NSTabView?
+  
+  private var currentPageIndex: Int {
+    guard let tabView = tabView, let selectedItem = tabView.selectedTabViewItem else { return 0 }
+    return tabView.indexOfTabViewItem(selectedItem)
+  }
+  
+  private var selectedWizard: CreationWizard? {
+    guard let itemIndex = outline?.selectedRow,
+      let wizard = outline?.item(atRow: itemIndex) as? CreationWizard
+    else {
+      return nil
+    }
+    return wizard
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    outline?.delegate = self
+    outline?.dataSource = self
+    outline?.reloadData()
+    outline?.expandItem(WizardsManager.shared)
+    
+    self.nextButton?.isEnabled = false
+    self.previousButton?.isEnabled = false
+  }
+  
+  
+  @IBAction func canselButtonDidClick(_ sender: Any?) {
+    closeWizard()
+  }
+  
+  @IBAction func nextButtonDidClick(_ sender: Any?) {
+    guard let wizard = selectedWizard else { return }
+    if wizard.wizardPages.isEmpty {
+      wizard.create { [weak self] in
+        self?.closeWizard()
+      }
+    } else {
+      if currentPageIndex == 0 {
+        setupPages(for: wizard)
+      }
+      showNextPage()
+    }
+  }
+  
+  @IBAction func prevButtonDidClick(_ sender: Any?) {
+    showPreviousePage()
+  }
+  
+  private func closeWizard() {
+    NSApp.stopModal()
+    (self.view.window as? NSPanel)?.orderOut(nil)
+  }
+  
+  private func setupPages(for wizard: CreationWizard) {
+    tabView?.tabViewItems.forEach{ item in
+      //remove all pages except first
+      guard item != mainTabViewItem else { return }
+      tabView?.removeTabViewItem(item)
+    }
+    
+    for page in wizard.wizardPages {
+      //realease old data
+      page.clearPage()
+      let tabViewItem = NSTabViewItem(identifier: nil)
+      tabViewItem.view = page
+      tabView?.addTabViewItem(tabViewItem)
+    }
+  }
+  
+  func showNextPage() {
+    let nextPageIndex = currentPageIndex + 1
+    guard nextPageIndex < tabView!.numberOfTabViewItems else {
+      selectedWizard?.create { [weak self] in
+        self?.closeWizard()
+      }
+      return
+    }
+    
+    //change header title
+    label?.stringValue = "Choose option for new project:"
+    //open next page
+    tabView!.selectTabViewItem(at: nextPageIndex)
+    
+    
+    if currentPageIndex != 0 {
+      previousButton?.isEnabled = true
+    }
+    
+    //last wizard page
+    if currentPageIndex == tabView!.numberOfTabViewItems - 1 {
+      nextButton?.title = "Create"
+    }
+    
+    pageChanged()
+  }
+  
+  func showPreviousePage() {
+    let previousePageIndex = currentPageIndex - 1
+    guard previousePageIndex >= 0 else {
+      return
+    }
+    
+    tabView!.selectTabViewItem(at: previousePageIndex)
+    nextButton?.title = "Next"
+    
+    //main wizard page
+    if currentPageIndex == 0 {
+      label?.stringValue = "Choose a template:"
+      previousButton?.isEnabled = false
+    }
+    
+    pageChanged()
+  }
+  
+  func pageChanged() {
+    guard currentPageIndex > 0 else {
+      nextButton?.isEnabled = true
+      return
+    }
+    
+    if let page = tabView?.selectedTabViewItem?.view as? WizardPage {
+      //Current page should be valid in order to go next step
+      nextButton?.isEnabled = page.isValid
+      page.validationHandler = { [weak self] isValid in
+        self?.nextButton?.isEnabled = isValid
+      }
+    }
+  }
+}
+
+
+
+extension WizardView: NSOutlineViewDataSource {
+  public func outlineView(_ outlineView: NSOutlineView, child index: Int, ofItem item: Any?) -> Any {
+    guard let item = item else { return WizardsManager.shared}
+    switch item {
+    case let manager as WizardsManager:
+      return manager.wizards[index]
+    default:
+      return self
+    }
+  }
+  
+  func outlineView(_ outlineView: NSOutlineView, numberOfChildrenOfItem item: Any?) -> Int {
+    guard let item = item else { return 1 }
+    switch item {
+    case let manager as WizardsManager:
+      return manager.wizards.count
+    default:
+      return 0
+    }
+  }
+  
+  public func outlineView(_ outlineView: NSOutlineView, isItemExpandable item: Any) -> Bool {
+    return self.outlineView(outlineView, numberOfChildrenOfItem: item) > 0
+  }
+}
+
+extension WizardView: NSOutlineViewDelegate {
+  
+  fileprivate enum CellIdentifiers {
+    static let headerCell = "HeaderCellID"
+    static let projectCell = "ProjectCellID"
+  }
+  
+  func outlineViewSelectionDidChange(_ notification: Notification) {
+    guard let outlineView = notification.object as? NSOutlineView,
+      let item = outlineView.item(atRow: outlineView.selectedRow),
+      let _ = item as? CreationWizard else { return }
+    self.nextButton?.isEnabled = true
+  }
+  
+  public func outlineView(_ outlineView: NSOutlineView, isGroupItem item: Any) -> Bool {
+    return item is WizardsManager
+  }
+  
+  public func outlineView(_ outlineView: NSOutlineView, shouldShowOutlineCellForItem item: Any) -> Bool {
+    return !(item is WizardsManager)
+  }
+  
+  public func outlineView(_ outlineView: NSOutlineView, shouldSelectItem item: Any) -> Bool {
+    return !(item is WizardsManager)
+  }
+  
+  public func outlineView(_ outlineView: NSOutlineView, viewFor tableColumn: NSTableColumn?, item: Any) -> NSView? {
+    switch item {
+    case let item as WizardsManager:
+      guard let view = outlineView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: CellIdentifiers.headerCell),
+                                            owner: self) as? NSTableCellView else { return nil }
+      view.textField?.stringValue = "PROJECTS"
+      return view
+    case let item as CreationWizard:
+      guard let view = outlineView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: CellIdentifiers.projectCell),
+                                            owner: self) as? NSTableCellView else { return nil }
+      view.textField?.stringValue = item.name
+      view.objectValue = item
+      return view
+    default:
+      return nil
+    }
+  }
+}
+
+
+class WizardPanel: NSPanel {
+  override var canBecomeKey: Bool {
+    true
+  }
+}
+
---
+++ Plugins/SwiftExtensions/Sources/SPMWizard.swift
@@ -0,0 +1,111 @@
+//
+//  SPMWizard.swift
+//  SwiftExtensions.plugin
+//
+//  Created by Danil Kristalev on 02.09.2020.
+//  Copyright © 2020 SCADE. All rights reserved.
+//
+
+import Cocoa
+import NimbleCore
+import SwiftExtensions
+
+class SPMWizard: CreationWizard {
+  var icon: Icon? {
+    nil
+  }
+  
+  var name: String {
+    "Swift Package"
+  }
+  
+  var wizardPages: [WizardPage] = []
+  
+  func create(onComplete: @escaping () -> Void) {
+    let spmView = SPMWizardView.loadFromNib()
+    let saveDialog = SPMSavePanel()
+    saveDialog.nameFieldLabel = "Save As: "
+    saveDialog.prompt = "Create"
+    saveDialog.nameFieldStringValue = "MyLibrary"
+    saveDialog.accessoryView = spmView
+    saveDialog.beginSheetModal(for: NSApp.keyWindow!){ result in
+      if result.rawValue == NSApplication.ModalResponse.OK.rawValue {
+        guard let directoryURL = saveDialog.directoryURL, let directoryPath = Path(url: directoryURL) else {
+          onComplete()
+          return
+        }
+        let projectName = saveDialog.nameFieldStringValue
+        let projectPath = directoryPath/projectName
+        
+        //create project folder
+        try? projectPath.mkdir()
+        //generate SPM package
+        let proc = Process()
+        proc.currentDirectoryURL = projectPath.url
+        let toolchain = SKLocalServer.swiftToolchain
+        if !toolchain.isEmpty {
+          proc.executableURL = URL(fileURLWithPath: "\(toolchain)/usr/bin/swift")
+        } else {
+          proc.executableURL = URL(fileURLWithPath: "/usr/bin/swift")
+        }
+        
+        let createGit = spmView.gitCheckBox!.state == .on
+        let type = SPMProjectType(rawValue: spmView.typePopup!.selectedItem!.title)!
+        proc.arguments = ["package", "init", "--type", type.commandType]
+        //hide pacakge output
+        proc.standardOutput = Pipe()
+        proc.terminationHandler = {[weak self] _ in
+          guard let self = self else { return }
+          //If need to create git repo
+           if createGit {
+            let git = self.setupGit(location: projectPath)
+            git(["init"])
+            git(["add", "*"])
+            git(["commit", "-m", "\"Initial Commit\""])
+          }
+          
+          //open created folder in current workbench
+          if let folder = Folder(path: projectPath) {
+            DispatchQueue.main.async {
+              NSApp.currentWorkbench?.project?.add(folder)
+            }
+          }
+        }
+        try? proc.run()
+      }
+      onComplete()
+    }
+    
+  }
+  
+  func setupGit(location: Path) -> ([String]) -> Void {
+    return { command in
+      let proc = Process()
+      proc.currentDirectoryURL = location.url
+      proc.executableURL = URL(fileURLWithPath: "/usr/bin/git")
+      proc.arguments = command
+      //hide git output
+      proc.standardOutput = Pipe()
+      try? proc.run()
+      proc.waitUntilExit()
+    }
+  }
+  
+  enum SPMProjectType: String {
+    case library = "Library"
+    case executable = "Executable"
+    case empty = "Empty"
+    case manifest = "Manifest"
+    case systemModule = "System-module"
+    
+    var commandType: String {
+      self.rawValue.prefix(1).lowercased() + self.rawValue.dropFirst()
+    }
+  }
+}
+
+private class SPMSavePanel: NSSavePanel {
+  override var isExpanded: Bool {
+    false
+  }
+}
