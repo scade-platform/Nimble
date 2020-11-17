d6a94a98a998aa0016c1cb02ea51ba43c63c77a9
shem <vsv@scade.io>
2020-11-17 18:50:31 +0200

    add zoomIn and zoomOut actions for CodeEditor (#171)

---
+++ Packages/NimbleCore/Sources/NimbleCore/Editor.swift
@@ -0,0 +1,50 @@
+import Cocoa
+
+///TODO: avoid constraining the protocol to the NSViewController
+public protocol WorkbenchEditor: NSViewController {
+  ///TODO: replace by Commands
+  // Shown within the app's main menu
+  static var editorMenu: NSMenu? { get }
+
+  var workbench: Workbench? { get }
+
+  var statusBarItems: [WorkbenchStatusBarItem] { get }
+  
+  @discardableResult
+  func focus() -> Bool
+  
+  func publish(diagnostics: [Diagnostic])
+  
+  func didOpenDocument(_ document: Document)
+}
+
+
+public extension WorkbenchEditor {
+  static var editorMenu: NSMenu? { return nil }
+
+  var workbench: Workbench? {
+    return view.window?.windowController as? Workbench
+  }
+
+  var statusBarItems: [WorkbenchStatusBarItem] { return [] }
+  
+  func focus() -> Bool {
+    return view.window?.makeFirstResponder(view) ?? false
+  }
+  
+  func publish(diagnostics: [Diagnostic]) { }
+  
+  func didOpenDocument(_ document: Document) { }
+}
+
+// MARK: - Editor Command Actions
+
+public protocol WorkbenchEditorZoomSuppot {
+
+  func zoomIn()
+
+  func zoomOut()
+}
+
+
+
