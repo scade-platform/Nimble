37434e9516faf092f99951595d818a843e509058
Danil Kristalev <kristalev.danil@gmail.com>
2021-10-14 22:38:09 +0700

    Add Apache v2 license header to files

--- Plugins/CodeEditor/CodeEditor/Sources/CodeEditorTextView.swift
+++ Plugins/CodeEditor/CodeEditor/Sources/CodeEditorTextView.swift
@@ -2,8 +2,20 @@
 //  CodeEditorTextView.swift
 //  CodeEditor
 //
-//  Created by Mark Goldin on 25/06/2019.
-//  Copyright © 2019 SCADE. All rights reserved.
+//  Copyright © 2021 SCADE Inc. All rights reserved.
+//
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  https://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 //
 
 import Cocoa
