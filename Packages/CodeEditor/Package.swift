f7aad770a8034feb50d392e01b956d0984d02acc
Kristalev Danil <kristalev.danil@gmail.com>
2022-09-13 12:50:12 +0700

    Update deployment target to 10.15.1

--- Packages/CodeEditor/Package.swift
+++ Packages/CodeEditor/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "CodeEditor",    
     platforms: [
-      .macOS(.v10_14),
+      .macOS(.v10_15),
     ],
     products: [        
       .library(name: "CodeEditor", type: .dynamic, targets: ["CodeEditor"]),
